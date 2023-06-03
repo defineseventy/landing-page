d93a0bd1ad9c60b6e34d4ccfc7b902345a04e1bd
defineseventy <jacweizhen6679@gmail.com>
2023-06-03 17:57:46 +0800

    Update index.js
    Added typewriter effect

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ function showOrHide() {
       c()
     }
   }
+let i = 0;
+let txt = 'Continue to try, seek help when you get stuck, have fun and you will be rewarded when your code finally works.';
+let txt1 = '~ Jacqueline27 (taken from my very first article on Hashnode)';
+let speed = 60;
+function typeWriter() {
+    if (i < txt.length && i < txt1.length) {
+        document.getElementById("quote").innerHTML += txt.charAt(i);
+        document.getElementById("quote").innerHTML += txt1.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
